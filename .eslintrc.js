e757c6d0a03baf9a9ed87e0db7f3d0ea828dba37
Chris Carrick <chris@crcarrick.dev>
2024-01-31 00:06:50 -0500

    Set up the project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.build.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/semi': 'off',
+  },
+}
