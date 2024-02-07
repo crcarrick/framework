9f0cb7ba6766fe810d0d8a7b59bdb50f16b97dd1
Chris Carrick <chris@crcarrick.dev>
2024-02-07 16:42:58 -0500

    Upgrade to react experimental channel

--- example/src/pages/home/page.tsx
+++ example/src/pages/home/page.tsx
@@ -1,15 +1,5 @@
-import { useState } from 'react'
-
 import { Header } from '../../components/Header.js'
 
 export default function HomePage() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      <Header>Home</Header>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  )
+  return <Header>Home</Header>
 }
