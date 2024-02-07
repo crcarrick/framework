d263b44d76cd60ecaba3cb57d11799e8e8c15c7d
Chris Carrick <chris@crcarrick.dev>
2024-02-07 02:14:23 -0500

    Add example of hydrated client component

---
+++ example/src/pages/count/[initialCount]/page.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import type { GetServerSideProps, InferServerSideProps } from '@framework/types'
+
+interface Params {
+  initialCount: string
+}
+
+export const getServerSideProps = (({ params }) => {
+  return Promise.resolve({
+    initialCount: parseInt(params.initialCount, 10),
+  })
+}) satisfies GetServerSideProps<Params>
+
+type CounterProps = InferServerSideProps<typeof getServerSideProps>
+
+export default function Counter({ initialCount }: CounterProps) {
+  const [count, setCount] = useState(initialCount)
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </div>
+  )
+}
