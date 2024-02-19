7cdf902af2a77a69675b0d0dc4ea5fbe414c8138
Chris Carrick <chris@crcarrick.dev>
2024-02-19 13:30:57 -0500

    Use shared eslint-config and tsconfig packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  extends: ['@crcarrick/eslint-config'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: [
@@ -9,74 +9,5 @@ module.exports = {
       './packages/*/tsconfig.json',
       './example/tsconfig.json',
     ],
-  },
-  plugins: [
-    '@eslint-community/eslint-comments',
-    '@typescript-eslint',
-    'import',
-    'react',
-    'react-hooks',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@eslint-community/eslint-comments/recommended',
-  ],
-  rules: {
-    semi: 'off',
-    'no-unused-vars': 'off',
-    '@eslint-community/eslint-comments/no-unused-disable': 'error',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-        },
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-        ],
-      },
-    ],
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: { '{}': false },
-        extendDefaults: true,
-      },
-    ],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/semi': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-      },
-    ],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-  },
-  settings: {
-    'import/internal-regex': '^metaframework/',
-    'import/parsers': {
-      '@typescript/eslint-parser': ['.ts', '.tsx'],
-    },
-    react: {
-      version: 'detect',
-    },
   },
 }
